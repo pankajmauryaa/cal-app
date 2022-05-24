a68e16c46cf931b99612739ddfa877ab37383ca7
pankajmauryaa <panmaury@cisco.com>
2022-05-24 15:21:21 +0530

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events  = [
+    {
+        title: "sandbox-scheduler-1",
+        start: new Date(2022, 5, 21),
+        end: new Date(2022, 5, 24),
+    },
+    {
+        title: "sandbox-scheduler-2",
+        start: new Date(2022, 6, 7),
+        end: new Date(2022, 6, 10),
+    }
+];
+
+export default function Main() {
+
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent]);
+    }
+
+    return (
+        <div>
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        </div>
+    );
+}
